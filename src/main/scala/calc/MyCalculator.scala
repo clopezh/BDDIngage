0fdebd62f3c89ea955368579c22953cda637399e
AlanMunozN <amunoz@intersysconsulting.com>
2018-10-03 12:27:50 -0500

    Alan´s hw

--- src/main/scala/calc/MyCalculator.scala
+++ src/main/scala/calc/MyCalculator.scala
@@ -1,4 +1,5 @@
 package calc
+import scala.math._
 
 class MyCalculator {
   def add(first: Int, second: Int): Int = {
@@ -7,4 +8,22 @@ class MyCalculator {
   def sub(first: Int, second: Int): Int = {
       first - second
   }
+  def percentage(first: Int, second: Int): Int = {
+    if (first!=0){
+      first * second / 100
+    }
+    else{
+      0
+    }
+
+  }
+  def logBase10(first: Double): Double = {
+    if (first!=0) {
+      log10(first)
+    }
+    else{
+      0
+    }
+  }
+
 }
--- src/test/scala/steps/StepDefinitions.scala
+++ src/test/scala/steps/StepDefinitions.scala
@@ -11,10 +11,16 @@ import scala.collection.mutable.ArrayBuffer
 class StepDefinitions extends ScalaDsl with EN {
   var calc: MyCalculator = _
   var result: ArrayBuffer[Int] = ArrayBuffer[Int]()
+  var resultLog: ArrayBuffer[Double] = ArrayBuffer[Double]()
 
   Given("""^my calculator is running$"""){ () =>
     calc = new MyCalculator
     result = ArrayBuffer[Int]()
+  }
+
+  Given("""^my calculator is running into log10$"""){ () =>
+    calc = new MyCalculator
+    resultLog = ArrayBuffer[Double]()
   }
 
   When("""^I add the following numbers$"""){ (numbers:DataTable) =>
@@ -34,16 +40,13 @@ class StepDefinitions extends ScalaDsl with EN {
   When("""^I subtract the following numbers$"""){ (numbers:DataTable) =>
     val list = numbers.asMaps(classOf[String], classOf[String])
     var i = 0
-    while ( {
-      i < list.size
-    }) {
+    while (  i < list.size  ) {
       result.append(calc.sub(list.get(i).get("firstNum").toInt, list.get(i).get("secondNum").toInt))
 
-      {
-        i += 1; i - 1
-      }
+        i += 1;
     }
   }
+
 
   Then("""^result should be equal to$"""){ (results:DataTable) =>
     var i = 0;
@@ -52,4 +55,42 @@ class StepDefinitions extends ScalaDsl with EN {
       i += 1
     }
   }
+
+  When("""^I take a percentage of the following numbers$"""){ (numbers:DataTable) =>
+    val list = numbers.asMaps(classOf[String], classOf[String])
+    var i = 0
+    while ( {
+      i < list.size
+    }) {
+      val unit: Unit = result.append(calc.percentage(list.get(i).get("firstNum").toInt, list.get(i).get("secondNum").toInt))
+      i += 1
+      i - 1
+
+    }
+  }
+
+  Then("""^result should be equal to log10$"""){ (results:DataTable) =>
+    var i = 0;
+    for (row <- results.asMaps(classOf[String], classOf[String])) {
+      assert(resultLog(i) == row.get("resultLog").toDouble, "Incorrect result of calculator computation")
+      i += 1
+    }
+  }
+
+  When("""^I take log10$"""){ (numbers:DataTable) =>
+    val list = numbers.asMaps(classOf[String], classOf[String])
+    var i = 0
+    while ( {
+      i < list.size
+    }) {
+      resultLog.append(calc.logBase10(list.get(i).get("firstNum").toDouble))
+
+      i += 1
+      i - 1
+
+    }
+  }
+
+
 }
+
