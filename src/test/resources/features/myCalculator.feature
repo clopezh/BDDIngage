a04521a2e343a8890dd1dc10d16599058d5f0e12
JoseBobadilla20 <josetriunfobobadillaguzman@MacBook-Pro-de-Jose.local>
2019-03-22 12:48:09 -0600

    Triunfo´s Homework

--- src/test/resources/features/myCalculator.feature
+++ src/test/resources/features/myCalculator.feature
@@ -24,3 +24,62 @@ Feature: My Calculator
       | 2      |
       | 7      |
 
+  @wip
+  Scenario: Should get the log of a positive numbers
+    Given my calculator is running
+    When I log a number by a number base
+      | Number   | Base      |
+      | 4        | 5         |
+      | 10       | 8         |
+    Then result should be equal to
+      | result       |
+      | 0.86135      |
+      | 1.10730      |
+
+  @wip
+  Scenario: Should multiply two positive numbers
+    Given my calculator is running
+    When I multiply the following numbers
+      | firstNum | secondNum |
+      | 2        | 2         |
+      | 9        | 3         |
+    Then result should be equal to
+      | result       |
+      | 4            |
+      | 27           |
+
+  @wip
+  Scenario: Should divide two positive numbers
+    Given my calculator is running
+    When I divide the following numbers
+      | firstNum | secondNum |
+      | 9        | 9         |
+      | 5        | 3         |
+    Then result should be equal to
+      | result       |
+      | 1            |
+      | 1.66666      |
+
+  @wip
+  Scenario: Should power two positive numbers
+    Given my calculator is running
+    When I power the following numbers
+      | Base     | Exponent |
+      | 3        | 3        |
+      | 5        | 3        |
+    Then result should be equal to
+      | result    |
+      | 27        |
+      | 125       |
+
+  @wip
+  Scenario: Should get the square root of a positive number
+    Given my calculator is running
+    When I get the square root of a positive following number
+      | firstNum |
+      | 7        |
+      | 25       |
+    Then result should be equal to
+      | result   |
+      | 2.64575  |
+      | 5        |
--- src/test/scala/steps/StepDefinitions.scala
+++ src/test/scala/steps/StepDefinitions.scala
@@ -10,11 +10,11 @@ import scala.collection.mutable.ArrayBuffer
 
 class StepDefinitions extends ScalaDsl with EN {
   var calc: MyCalculator = _
-  var result: ArrayBuffer[Int] = ArrayBuffer[Int]()
+  var result: ArrayBuffer[Double] = ArrayBuffer[Double]()
 
   Given("""^my calculator is running$"""){ () =>
     calc = new MyCalculator
-    result = ArrayBuffer[Int]()
+    result = ArrayBuffer[Double]()
   }
 
   When("""^I add the following numbers$"""){ (numbers:DataTable) =>
@@ -48,8 +48,80 @@ class StepDefinitions extends ScalaDsl with EN {
   Then("""^result should be equal to$"""){ (results:DataTable) =>
     var i = 0;
     for (row <- results.asMaps(classOf[String], classOf[String])) {
-      assert(result(i) == row.get("result").toInt, "Incorrect result of calculator computation")
+      assert(result(i) == row.get("result").toDouble, "Incorrect result of calculator computation")
       i += 1
     }
   }
+
+  When("""^I multiply the following numbers$"""){ (numbers:DataTable) =>
+    val list = numbers.asMaps(classOf[String], classOf[String])
+    var i = 0
+    while ( {
+      i < list.size
+    }) {
+      result.append(calc.mul(list.get(i).get("firstNum").toInt, list.get(i).get("secondNum").toInt))
+
+      {
+        i += 1; i - 1
+      }
+    }
+  }
+
+  When("""^I divide the following numbers$"""){ (numbers:DataTable) =>
+    val list = numbers.asMaps(classOf[String], classOf[String])
+    var i = 0
+    while ( {
+      i < list.size
+    }) {
+      result.append(calc.div(list.get(i).get("firstNum").toDouble, list.get(i).get("secondNum").toDouble))
+
+      {
+        i += 1; i - 1
+      }
+    }
+  }
+
+  When("""^I get the square root of a positive following number$"""){ (numbers:DataTable) =>
+    val list = numbers.asMaps(classOf[String], classOf[String])
+    var i = 0
+    while ( {
+      i < list.size
+    }) {
+      result.append(calc.sqrt(list.get(i).get("firstNum").toDouble))
+
+      {
+        i += 1; i - 1
+      }
+    }
+  }
+
+  When("""^I power the following numbers$"""){ (numbers:DataTable) =>
+    val list = numbers.asMaps(classOf[String], classOf[String])
+    var i = 0
+    while ( {
+      i < list.size
+    }) {
+      result.append(calc.pow(list.get(i).get("Base").toInt, list.get(i).get("Exponent").toInt))
+
+      {
+        i += 1; i - 1
+      }
+    }
+  }
+
+
+  When("""^I log a number by a number base$"""){ (numbers:DataTable) =>
+    val list = numbers.asMaps(classOf[String], classOf[String])
+    var i = 0
+    while ( {
+      i < list.size
+    }) {
+      result.append(calc.log(list.get(i).get("Number").toDouble, list.get(i).get("Base").toInt))
+
+      {
+        i += 1; i - 1
+      }
+    }
+  }
+
 }
